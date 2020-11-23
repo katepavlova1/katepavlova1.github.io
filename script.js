11b554a91f4baa4337e68722a7139aae57e408fd
Kate Pavlova <ekaterina.vik.pavlova@gmail.com>
2020-11-23 12:30:21 -0500

    html structure done, started the business logics

---
+++ script.js
@@ -0,0 +1,10 @@
+const startButton = document.getElementsByClassName("start-btn")
+startButton.addEventListener('click', startGame())
+
+function startGame(){
+    console.log ('started')
+}
+
+// function nextQuestion(){}
+
+// function selectAnswer(){}
